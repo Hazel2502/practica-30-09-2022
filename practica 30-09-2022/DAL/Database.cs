859d96f510730a2bb2e3730a586f8d437b3ef72a
Hazel Granados <hazelbgranados@gmail.com>
2022-10-05 11:58:54 -0900

    Agregar archivos de proyecto.

---
+++ practica 30-09-2022/DAL/Database.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using practica_30_09_2022.Properties;
+using System.Configuration;
+
+namespace practica_30_09_2022.DAL
+{
+    public class Database
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.ConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+        
+        public bool testConnection()
+        {
+            SqlConnection Con = this.getConnection();
+            Con.Open();
+            if (Con.State == System.Data.ConnectionState.Open)
+            {
+                Con.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            public bool testInsert(string nombre, string ubicacion)
+            {
+                try
+                {
+                    SqlConnection Con = this.getConnection();
+                    using (SqlCommand cmd = Con.CreateCommand())
+                    {
+                        Con.Open();
+                        cmd.CommandText = "INSERT INTO Sedes (nombre, ubicacion) VALUES (@nom, @ub);";
+                        cmd.Parameters.AddWithValue("@nom", nombre);
+                        cmd.Parameters.AddWithValue("@ub", ubicacion);
+                        cmd.ExecuteNonQuery();
+                        Con.Close();
+
+                        return true;
+
+
+                    }
+
+
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+                
+
+
+            
+            
+        }
+
+    }
+}
---
+++ practica 30-09-2022/DLL/SedesBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_30_09_2022.DLL
+{
+    class SedesBLL
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicaciones { get; set; }
+
+    }
+}
